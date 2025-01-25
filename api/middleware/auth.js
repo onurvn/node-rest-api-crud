62a5271a610a956095f70549b619a351170a87bd
Onur Avan <avanonur@hotmail.com>
2025-01-26 01:13:04 +0300

    auth middleware created

---
+++ api/middleware/auth.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+
+        let decodedData;
+
+        if (token) {
+            decodedData = jwt.verify(token, process.env.SECRET_TOKEN);
+
+            req.userId = decodedData?.id;
+        } else {
+            decodedData = jwt.decode(token);
+
+            req.userId = decodedData?.sub;
+        }
+
+        next();
+
+    } catch (error) {
+        console.log("error" + error);
+
+    }
+};
+
+module.exports = auth;
